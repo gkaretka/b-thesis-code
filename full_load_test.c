43026eec97834861b5d73defb7fc3dbf19f9d0fa
Gregor Karetka <karetka.gregor@gmail.com>
2022-04-27 21:32:12 +0200

    Update full_load_test.c

--- full_load_test.c
+++ full_load_test.c
@@ -10,10 +10,11 @@ int main()
     ADF4351_register_t regs;
 
     // set 100.154 MHz
+    // Warning - this sets output to full +5 dBm power.
     Pll_ADF4351_set_frequency(40000000, &vals);             // calculate values
     Pll_ADF4351_show_RF_settings(&vals);                    // show calculated values
     Pll_ADF4351_load_default_settings(&settings);           // load default settings
     Pll_ADF4351_fill_registers(&vals, &settings, &regs);    // fill registers with data
-    
+
     Pll_ADF4351_display_registers_hex(&regs);               // Display registers
 }
